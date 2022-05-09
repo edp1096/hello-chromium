40fcb8614f747213d6c0a3164007431be99c1226
edp1096 <edp1096@naver.com>
2022-05-09 15:29:48 +0900

    Add monaco

--- run.cmd
+++ run.cmd
@@ -7,4 +7,5 @@ rmdir %cd%\user /q/s > nul 2>nul
 @REM --no-first-run : Must not use
 @REM --kiosk --print-to-pdf-no-header
 @REM  --disable-windows10-custom-titlebar --disable-background-timer-throttling --disable-extensions --disable-sync --disable-logging --disable-speech-api --enable-local-file-accesses --ignore-autocomplete-off-autofill --in-process-gpu --no-experiments --no-proxy-server --webrtc-max-cpu-consumption-percentage=100
-"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --user-data-dir=%cd%/user --app=file:///%cd%/index.html --window-position=0,0 --window-size=800,600 --ash-force-desktop --disable-translate --disable-background-mode --disable-frame-rate-limit --disable-gpu-vsync --no-initial-navigation --no-default-browser-check
+@REM --ash-force-desktop --disable-translate --disable-background-mode --disable-frame-rate-limit --disable-gpu-vsync
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --user-data-dir=%cd%/user --app=file:///%cd%/index.html --window-position=0,0 --window-size=1024,768 --no-initial-navigation --no-default-browser-check
